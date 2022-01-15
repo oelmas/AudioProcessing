114e5f495e455ad7f40df8663378cb73e07b6be4
Öner Elmas <oner.elmas@hotmail.com>
2022-01-15 22:48:12 +0300

    Remove sound data one cahnnel respectively. and write new sound to video file

--- removeSilence_bad_version1.py
+++ removeSilence_bad_version1.py
@@ -1,4 +1,4 @@
-from cProfile import label
+from email.mime import audio
 import numpy as np
 import matplotlib.pyplot as plt
 from moviepy.editor import *
@@ -16,6 +16,7 @@ print(clip_audio.size)
 margin_pos = 0.05       # volume cut off margin at positive
 margin_neg = -0.05      # volume cut off margin at negative
 
+'''
 
 # PLot the volume levels and margins
 plt.plot(clip_audio, 'b', linewidth=0.5, label='Volume')
@@ -27,3 +28,27 @@ plt.ylabel('amplitude',fontsize=15)
 plt.grid(True)
 plt.legend(loc='upper right', fontsize='small')
 plt.show()
+
+
+# Remove one channel !
+
+clip_audio[:,0] = 0
+print(clip_audio)
+
+audio_object = AudioArrayClip(clip_audio, fps=44100)
+print(audio_object)
+
+audio_object.write_audiofile("just_right.wav")
+'''
+
+# Remove one channel !
+clip_audio[:,1] = 0
+print(clip_audio)
+
+audio_object = AudioArrayClip(clip_audio, fps=44100)
+print(audio_object)
+
+audio_object.write_audiofile("just_left.wav")
+
+clip.audio = audio_object
+clip.write_videofile("video_modified_sound.mp4",codec='libx264', fps=30)
